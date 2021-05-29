90e9e2ec73d8161b005a8eff18e8ef45a2ca3fae
ahmetberkinc <ahmetberkincoglu@gmail.com>
2021-05-29 23:03:54 +0300

    Google IMA SDK tests.

---
+++ app/src/main/java/com/example/imasdktest/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.imasdktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.multidex.MultiDex;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ext.ima.ImaAdsLoader;
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
+import com.google.android.exoplayer2.source.MediaSourceFactory;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private ImaAdsLoader adsLoader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MultiDex.install(this);
+
+        playerView = findViewById(R.id.player_view);
+
+        // Create an AdsLoader.
+        adsLoader = new ImaAdsLoader.Builder(/* context= */ this).build();
+
+
+    }
+    private void releasePlayer() {
+        adsLoader.setPlayer(null);
+        playerView.setPlayer(null);
+        player.release();
+        player = null;
+    }
+
+    private void initializePlayer() {
+        // Set up the factory for media sources, passing the ads loader and ad view providers.
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(this, Util.getUserAgent(this, getString(R.string.app_name)));
+
+        MediaSourceFactory mediaSourceFactory =
+                new DefaultMediaSourceFactory(dataSourceFactory)
+                        .setAdsLoaderProvider(unusedAdTagUri -> adsLoader)
+                        .setAdViewProvider(playerView);
+
+        // Create a SimpleExoPlayer and set it as the player for content and ads.
+        player = new SimpleExoPlayer.Builder(this).setMediaSourceFactory(mediaSourceFactory).build();
+        playerView.setPlayer(player);
+        adsLoader.setPlayer(player);
+
+        // Create the MediaItem to play, specifying the content URI and ad tag URI.
+        Uri contentUri = Uri.parse(getString(R.string.content_url));
+        Uri adTagUri = Uri.parse(getString(R.string.ad_tag_url));
+        MediaItem mediaItem = new MediaItem.Builder().setUri(contentUri).setAdTagUri(adTagUri).build();
+
+        // Prepare the content and ad to be played with the SimpleExoPlayer.
+        player.setMediaItem(mediaItem);
+        player.prepare();
+
+        // Set PlayWhenReady. If true, content and ads will autoplay.
+        player.setPlayWhenReady(false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+            if (playerView != null) {
+                playerView.onResume();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer();
+            if (playerView != null) {
+                playerView.onResume();
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            if (playerView != null) {
+                playerView.onPause();
+            }
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            if (playerView != null) {
+                playerView.onPause();
+            }
+            releasePlayer();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        adsLoader.release();
+    }
+}
